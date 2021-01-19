4e129248fb41f1018221b2046259123872482b96
Tom Stitt <tomstitt@hey.com>
2021-01-19 12:50:56 -0800

    Update glvis.js:
    Build Info:
    emscripten: 2.0.11
    mfem:       v4.2-511-g9d236c575
    glvis:      v4.0-64-g5f0dca7

--- src/glvis.js
+++ src/glvis.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32a674994b271fd2d873dfc72beac8e9b6b448982614ca5911105f007ba1f5ae
-size 5363066
+oid sha256:12583ef146da674f478df793692adb9aafde905a850a3f62bd2db45c63fb7d87
+size 5365710
--- src/versions.js
+++ src/versions.js
@@ -1,5 +1,5 @@
 const versions = {
   emscripten: "2.0.11",
-  mfem: "v4.2-370-gf4c2758e8",
-  glvis: "v4.0-57-g585e34f",
+  mfem: "v4.2-511-g9d236c575",
+  glvis: "v4.0-64-g5f0dca7",
 };
