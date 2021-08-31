445e315d68fd4c5f2183376b476ff58b9f171516
Tom Stitt <tomstitt@hey.com>
2021-08-31 10:36:07 -0700

    updates after some bugfixes

--- src/glvis.js
+++ src/glvis.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f7ec015b378694bfb88e4046d5407639babdb630d90e8a9a124a96f32882eb2
-size 6496037
+oid sha256:06c31ed2b0120d3659eda6b3963c60a1c3b0eb35203bddd87d46444be6d0619d
+size 6546210
