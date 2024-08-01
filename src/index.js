429560d2e5edff130b1df020a3ce70c8b8bc9eda
Sam <samuelpmish@users.noreply.github.com>
2024-08-01 16:08:52 -0700

    support importing .mesh files, add error message

--- src/index.js
+++ src/index.js
@@ -214,9 +214,19 @@
     }
 
     async loadStream(e) {
-      const filename = e.target.files[0];
-      const data = await new Response(filename).text();
-      await this.display(data);
+      const file = e.target.files[0];
+      const data = await new Response(file).text();
+      const extension = file.name.split('.').pop();
+
+      if (extension != "mesh" || extension != "saved") {
+        console.log("Error: only .mesh and .saved files are supported");
+      }
+      
+      if (extension == "mesh") {
+        await this.display("mesh\n" + data);
+      } else {
+        await this.display(data);
+      }
     }
 
     setTouchDevice(status) {
